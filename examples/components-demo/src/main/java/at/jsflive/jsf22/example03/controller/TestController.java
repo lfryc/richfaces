543ebbd53a9cb61538d7a9e4a416c5965a9a1484
Lukas Fryc <lfryc@redhat.com>
2013-06-26 20:02:22 +0200

    sample for prototyping Autocomplete widget as Composite Component

---
+++ examples/components-demo/src/main/java/at/jsflive/jsf22/example03/controller/TestController.java
@@ -0,0 +1,17 @@
+package at.jsflive.jsf22.example03.controller;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class TestController {
+    private boolean collapsed = false;
+
+    public boolean isCollapsed() {
+        return collapsed;
+    }
+
+    public void setCollapsed(boolean collapsed) {
+        System.out.println("collaped");
+        this.collapsed = collapsed;
+    }
+}
